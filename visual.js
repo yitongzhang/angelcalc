d63849af938c586d4e29d1d70ed78127c9f51f07
Yitong <zha.yitong@gmail.com>
2017-03-19 23:57:32 -0700

    Almost done with react notes

--- visual.js
+++ visual.js
@@ -1,7 +1,71 @@
-// render list of note items
-class Welcome extends React.Component {
+var notesToRender=[];
+
+// render a single input field based on: noteName, noteField, noteID
+class FieldItem extends React.Component{
+	render(){
+		return(
+			<div className="inputFieldItem">
+				<div className="line"></div>
+				<input type="number" id={this.props.noteID} name={this.props.noteID} placeholder="0"/>
+				<label htmlFor={this.props.noteID}>{this.props.noteField}</label>
+			</div>
+		);
+	}
+}
+
+// render a single note item based on: noteName, noteFields, noteIDs
+class NoteItem extends React.Component{
+	render(){
+		const noteName = this.props.noteName;
+		const noteFields = noteData[noteName].fields;
+		const noteIds = noteData[noteName].fieldId;
+
+		const listOfInputs = noteIds.map((field)=>
+	  		<div key={field.toString()}>
+	  			<FieldItem noteID={field} noteField={field}/>
+	  		</div>
+		);
+
+		return(
+			<article id={this.props.noteName} key={noteName.toString()}>
+    			<div className="articleTitle">
+					<h1>{this.props.noteName} <button>X</button></h1>
+					<h2>Input the note data here.</h2>
+    			</div>
+    			<form action="" className="inputFieldGroup">
+					{listOfInputs}
+    			</form>
+    		</article>
+		);
+	}
+}
+
+// render list of note items based on the notesToRender array
+class NoteList extends React.Component {
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+  	const notesToRender = this.props.notesToRender;
+  	const listOfNotes = notesToRender.map((note) =>
+  		<div key={note.toString()}>
+  			<NoteItem noteName={note}/>
+  		</div>
+  	);
+    return( 
+    	<div>{listOfNotes}</div>
+    );
   }
 }
-ReactDOM.render(<Welcome name="Sara" /> ,document.getElementById('insertNotesHere'));
+
+$('#addNewNote').on('input', function() {
+	var selectedNote = $("#addNewNote option:selected").val();
+	notesToRender.push(selectedNote);
+
+	console.log(selectedNote);
+	console.log(notesToRender);
+	console.log(noteData[selectedNote])
+	ReactDOM.render(<NoteList notesToRender={notesToRender}/> ,document.getElementById('insertNotesHere'));
+	
+	// make selector go back to default action
+});
+
+
+
